d0280b249c785aea4d62240862b2cccec1f841f4
Lars-Ove Karlsson <lars-ove.karlsson@iar.com>
2024-09-09 15:10:41 +0200

    WA: Added tmp workaround for variables in registers in semihost.c

--- arch/arm/core/cortex_m/semihost.c
+++ arch/arm/core/cortex_m/semihost.c
@@ -7,13 +7,41 @@
 
 #include <zephyr/arch/common/semihost.h>
 
+#if 1
+
+#ifdef __ICCARM__
+// IAR WA VAAK-88: __asm instead of __asm__
+
+#ifndef __asm__
+#define __asm__ __asm
+#endif
+
+// IAR WA VAAK-7: volatile instead of __volatile__
+
+#ifndef __volatile__
+#define __volatile__ volatile
+#endif
+
+// IAR WA VAAK-8: Using _Pragma location instead of __asm__
+
 long semihost_exec(enum semihost_instr instr, void *args)
 {
-	register unsigned int r0 __asm__ ("r0") = instr;
-	register void *r1 __asm__ ("r1") = args;
-	register int ret __asm__ ("r0");
+	int ret;
+	// IAR WA VAAK-8: Using specific registers 
 
-	__asm__ __volatile__ ("bkpt 0xab"
-			      : "=r" (ret) : "r" (r0), "r" (r1) : "memory");
+	__asm__ __volatile__("bkpt 0xab" : "=r0"(ret) : "r0"(instr), "r1"(args) : "memory");
 	return ret;
 }
+
+#else
+
+	long semihost_exec(enum semihost_instr instr, void *args)
+{
+	register unsigned int r0 __asm__("r0") = instr;
+	register void *r1 __asm__("r1") = args;
+	register int ret __asm__("r0");
+
+	__asm__ __volatile__("bkpt 0xab" : "=r"(ret) : "r"(r0), "r"(r1) : "memory");
+	return ret;
+}
+#endif
