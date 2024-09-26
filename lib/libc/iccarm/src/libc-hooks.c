8cf8e46351e82accd62a4af2abb3180fcbbcd41d
Peter Öhman <peter.ohman@iar.com>
2024-09-26 16:01:47 +0200

    Added a weak __write

--- lib/libc/iccarm/src/libc-hooks.c
+++ lib/libc/iccarm/src/libc-hooks.c
@@ -1,6 +1,4 @@
 /*
- * Copyright (c) 2021 Nordic Semiconductor ASA
- *
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -9,19 +7,49 @@
 
 static int _stdout_hook_default(int c)
 {
-	(void)(c);  /* Prevent warning about unused argument */
+  (void)(c);  /* Prevent warning about unused argument */
 
-	return EOF;
+  return EOF;
 }
 
 static int (*_stdout_hook)(int) = _stdout_hook_default;
 
 void __stdout_hook_install(int (*hook)(int))
 {
-	_stdout_hook = hook;
+  _stdout_hook = hook;
 }
 
 int fputc(int c, FILE *f)
 {
-	return (_stdout_hook)(c);
+  return (_stdout_hook)(c);
+}
+
+#pragma weak __write
+size_t __write(int handle,
+               const unsigned char *buf,
+               size_t bufSize)
+{
+  size_t nChars = 0;
+  /* Check for the command to flush all handles */
+  if (handle == -1)
+  {
+    return 0;
+  }
+  /* Check for stdout and stderr
+     (only necessary if FILE descriptors are enabled.) */
+  if (handle != 1 && handle != 2)
+  {
+    return -1;
+  }
+  for (/* Empty */; bufSize > 0; --bufSize)
+  {
+    int ret = (_stdout_hook)(*buf);
+    if (ret == EOF)
+    {
+      break;
+    }
+    ++buf;
+    ++nChars;
+  }
+  return nChars;
 }
