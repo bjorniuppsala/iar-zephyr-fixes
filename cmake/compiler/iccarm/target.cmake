49f31e5125119d1ba242de5457b2dab2dc5de88a
Lars-Ove Karlsson <lars-ove.karlsson@iar.com>
2024-10-08 16:40:02 +0200

    VAAK-207: Disabled a warning for when bitmask contains bits outside the size of the variable.

--- cmake/compiler/iccarm/target.cmake
+++ cmake/compiler/iccarm/target.cmake
@@ -95,6 +95,7 @@ list(APPEND TOOLCHAIN_C_FLAGS
   --diag_suppress=Pe1153  # declaration does not match its alias variable "xxx"
   --diag_suppress=Pe191  # type qualifier is meaningless on cast type
   --diag_suppress=Pe177  # function "xxx" was declared but never referenced
+  --diag_suppress=Pa182  # bit mask appears to contain significant bits that do not affect the result
 )
 
 if(CONFIG_ENFORCE_ZEPHYR_STDINT)
