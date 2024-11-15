0b3dec919ec47efaeaea0e5a29cb89ef62a14cd9
Lars-Ove Karlsson <lars-ove.karlsson@iar.com>
2024-11-16 00:17:50 +0100

    Updated toolchain include files

---
+++ include/zephyr/toolchain/iar.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2022 IAR Systems AB
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_TOOLCHAIN_IAR_H_
+#define ZEPHYR_INCLUDE_TOOLCHAIN_IAR_H_
+
+#ifdef __ICCARM__
+#include "iar/iccarm.h"
+#endif
+
+#endif /* ZEPHYR_INCLUDE_TOOLCHAIN_ICCARM_H_ */
