89e484964fc111dfc5b4a3acb5da2d1cdf52b83a
Lars-Ove Karlsson <lars-ove.karlsson@iar.com>
2024-11-18 13:43:24 +0100

    Suppressed warning Pe185  # dynamic initialization in unreachable code as it can show up depending on options/drivers you are using

--- cmake/compiler/iar/target.cmake
+++ cmake/compiler/iar/target.cmake
@@ -102,6 +102,7 @@ list(APPEND TOOLCHAIN_C_FLAGS
   --diag_suppress=Pe177  # function "xxx" was declared but never referenced
   --diag_suppress=Pa182  # bit mask appears to contain significant bits that do not affect the result
   --diag_suppress=Pa039  # use of address of unaligned structure member
+  --diag_suppress=Pe185  # dynamic initialization in unreachable code
 )
 
 if(CONFIG_ENFORCE_ZEPHYR_STDINT)
