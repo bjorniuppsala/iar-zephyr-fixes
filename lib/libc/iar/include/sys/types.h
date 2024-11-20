85a948f432885426dbcc637cb5634a66f0b4610e
Lars-Ove Karlsson <lars-ove.karlsson@iar.com>
2024-11-20 12:58:20 +0100

    Don't define clock_t in sys/types.h if it has already been defined

--- lib/libc/iar/include/sys/types.h
+++ lib/libc/iar/include/sys/types.h
@@ -11,6 +11,11 @@ typedef unsigned int mode_t;
 typedef signed long ssize_t;
 typedef int off_t;
 typedef __INT64_TYPE__ time_t;
+
+#if !defined(_CLOCK_T_DECLARED) && !defined(__clock_t_defined)
 typedef unsigned int clock_t;
+#define _CLOCK_T_DECLARED
+#define __clock_t_defined
+#endif
 
 #endif /* ZEPHYR_LIB_LIBC_ICCARM_INCLUDE_SYS_TYPES_H_ */
