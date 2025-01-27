2d8630278ebc81a3620cf94c73e805e9c4712711
Robin Kastberg <robin.kastberg@iar.com>
2025-01-27 10:43:12 +0000

    fix asm_inline.h

--- include/zephyr/arch/arm/asm_inline.h
+++ include/zephyr/arch/arm/asm_inline.h
@@ -17,7 +17,7 @@
 #if defined(__GNUC__) || defined(__ICCARM__)
 #include <zephyr/arch/arm/asm_inline_gcc.h>
 #else
-#include <arch/arm/asm_inline_other.h>
+#error Unknown toolchain in asm_inline.h
 #endif
 
 #endif /* ZEPHYR_INCLUDE_ARCH_ARM_ASM_INLINE_H_ */
